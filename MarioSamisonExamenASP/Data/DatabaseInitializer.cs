0057afecaf82630186118a4f38e4b3200319354f
Mario93Mechelen <r0334050@student.thomasmore.be>
2018-01-10 18:46:35 +0100

    changes

--- MarioSamisonExamenASP/Data/DatabaseInitializer.cs
+++ MarioSamisonExamenASP/Data/DatabaseInitializer.cs
@@ -38,13 +38,13 @@ namespace MarioSamisonExamenASP.Data
                 cars.Add(new Car { Color = "Grey", DateBought = Convert.ToDateTime("2010-10-12"), LicensePlate = "1-KBM-588", CarOwner = owners[0], CarType = cartypes[0] });
                 cars.Add(new Car { Color = "Red", DateBought = Convert.ToDateTime("2005-04-28"), LicensePlate = "1-HCP-481", CarOwner = owners[1], CarType = cartypes[2] });
                 cars.Add(new Car { Color = "Black", DateBought = Convert.ToDateTime("1991-12-27"), LicensePlate = "1-GDT-548", CarOwner = owners[1], CarType = cartypes[5] });
-                cars.Add(new Car { Color = "Red", DateBought = Convert.ToDateTime("2017-04-08"), LicensePlate = "1-HSY-688", CarOwner = owners[4], CarType = cartypes[3] });
+                cars.Add(new Car { Color = "Red", DateBought = Convert.ToDateTime("2017-04-08"), LicensePlate = "1-HSY-688", CarOwner = owners[3], CarType = cartypes[3] });
                 cars.Add(new Car { Color = "Yellow", DateBought = Convert.ToDateTime("2006-02-11"), LicensePlate = "1-TSY-471", CarOwner = owners[2], CarType = cartypes[0] });
                 cars.Add(new Car { Color = "Green", DateBought = Convert.ToDateTime("2000-10-15"), LicensePlate = "1-GTS-456", CarOwner = owners[3], CarType = cartypes[1] });
                 cars.Add(new Car { Color = "White", DateBought = Convert.ToDateTime("2007-05-29"), LicensePlate = "1-ABC-123", CarOwner = owners[2], CarType = cartypes[4] });
-                cars.Add(new Car { Color = "White", DateBought = Convert.ToDateTime("2015-07-17"), LicensePlate = "1-KIP-069", CarOwner = owners[5], CarType = cartypes[7] });
-                cars.Add(new Car { Color = "Blue", DateBought = Convert.ToDateTime("2016-11-02"), LicensePlate = "1-HOH-541", CarOwner = owners[0], CarType = cartypes[6] });
-                cars.Add(new Car { Color = "Black", DateBought = Convert.ToDateTime("2010-06-24"), LicensePlate = "1-PIS-561", CarOwner = owners[5], CarType = cartypes[6] });
+                cars.Add(new Car { Color = "White", DateBought = Convert.ToDateTime("2015-07-17"), LicensePlate = "1-KIP-069", CarOwner = owners[1], CarType = cartypes[0] });
+                cars.Add(new Car { Color = "Blue", DateBought = Convert.ToDateTime("2016-11-02"), LicensePlate = "1-HOH-541", CarOwner = owners[0], CarType = cartypes[1] });
+                cars.Add(new Car { Color = "Black", DateBought = Convert.ToDateTime("2010-06-24"), LicensePlate = "1-PIS-561", CarOwner = owners[3], CarType = cartypes[5] });
 
 
             //add newly created test data to database and save
